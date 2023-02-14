881fbd47cd1f96b2785fc5b8f44c95859072b1f3
OneOfTheInfiniteMonkeys <45082871+OneOfTheInfiniteMonkeys@users.noreply.github.com>
2023-02-14 00:28:54 +0000

    Empty secrets.py
    Beta release - empty reference file  - Requires configuring if network features required

---
+++ dist/secrets.py
@@ -0,0 +1,19 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+# Notes
+# Defining channel speeds access and reduces power if you know the WiFi Channel
+# Defining timeout sets the amount of time to wait for a WiFi access request, also affects power consumption
+# Change as needed and remove what you don't use
+
+secrets = {
+    'ssid' : '',
+    'password' : '',
+    'channel' : '',
+    'timeout' : '',
+    'aio_username' : '',
+    'aio_key' : '',
+    'broker' : 'io.adafruit.com',
+    'port' : 1883,
+    'timezone' : '',
+}
